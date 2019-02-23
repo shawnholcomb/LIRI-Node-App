fb72a8c40725ee46d1c77831ab816d7b35e6fc4d
Shawn Holcomb <sholcmb@gmail.com>
2019-02-22 21:32:33 -0600

    everything other than Spotify is working now

--- liri.js
+++ liri.js
@@ -3,77 +3,117 @@ require("dotenv").config();
 var keys = require("./keys");
 var axios = require("axios");
 var moment = require('moment');
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
 var fs = require("fs");
 
+// Grab command line arguments
 var type = process.argv[2];
 var search = process.argv.splice(3).join(" ");
 
+// Switch based on user input
 switch (type) {
     case "concert-this":
-        var artist = search;
-        axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-            .then(
-                function (response) {
-                    var details = response.data;
-                    details.forEach(function (element) {
-                        var date = moment(element.datetime).format('L');
-                        var concertResponse = "Venue: " + element.venue.name + "\nLocation: " + element.venue.city + ", " + element.venue.region + " " + element.venue.country + "\nDate: " + date + "\n------------------------------\n";
-                        console.log(concertResponse);
-                        fs.appendFile("log.txt", concertResponse, function (err) {
-                            if (err) {
-                                return console.log(err);
-                            }
-                        })
-                    })
-                })
+        concertThis()
         break;
-
     case "spotify-this-song":
-        var song;
-        if (!search) {
-            song = "The Sign"
-        } else {
-            song = search;
-            spotify
-                .search({ type: 'track', query: song })
-                .then(function (response) {
-                    var songResponse = (response)
-                    console.log(songResponse);
-                    fs.appendFile("log.txt", songResponse, function (err) {
+        spotifyThis()
+        break;
+    case "movie-this":
+        movieThis();
+        break;
+    case "do-what-it-says":
+        doThis();
+        break;
+}
+
+// Function for grabbing BandsInTown data
+function concertThis() {
+    var artist = search;
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(
+            function (response) {
+                var details = response.data;
+                details.forEach(function (element) {
+                    var date = moment(element.datetime).format('L');
+                    var concertResponse = "Venue: " + element.venue.name + "\nLocation: " + element.venue.city + ", " + element.venue.region + " " + element.venue.country + "\nDate: " + date + "\n------------------------------\n";
+                    console.log(concertResponse);
+                    fs.appendFile("log.txt", concertResponse, function (err) {
                         if (err) {
                             return console.log(err);
                         }
                     })
                 })
-                .catch(function (err) {
-                    console.log(err);
+            });
+};
+
+// Function for grabbing Spotify data
+function spotifyThis() {
+    var Spotify = require('node-spotify-api');
+    var spotify = new Spotify(keys.spotify);
+
+    var song;
+    if (!search) {
+        song = "The Sign";
+        getSong();
+    } else {
+        song = search;
+        getSong();
+    };
+
+    function getSong() {
+        spotify
+            .search({ type: 'track', query: song })
+            .then(function (response) {
+                var songResponse = (response)
+                console.log(songResponse);
+                fs.appendFile("log.txt", songResponse, function (err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                })
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    }
+};
+
+// Function for grabbing Movie data from OMDB
+function movieThis() {
+    var movie;
+    if (!search) {
+        movie = "Mr. Nobody";
+        getMovie();
+    } else {
+        movie = search;
+        getMovie();
+
+    }
+
+    function getMovie() {
+        axios.get("http://www.omdbapi.com/?t=" + movie + "&apikey=trilogy")
+            .then(
+                function (response) {
+                    var details = response.data;
+                    var movieResponse = "Title: " + details.Title + "\nYear: " + details.Year + "\nIMDB Rating: " + details.Ratings[0].Value + "\nRotten Tomatoes Rating: " + details.Ratings[1].Value + "\nCountry: " + details.Country + "\nLanguage: " + details.Language + "\nPlot: " + details.plot + "\nActors: " + details.Actors + "\n------------------------------\n";
+                    console.log(movieResponse);
+                    fs.appendFile("log.txt", movieResponse, function (err) {
+                        if (err) {
+                            return console.log(err);
+                        }
+                    })
                 });
-        }
-        break;
-    case "movie-this":
-        var movie;
-        if (!search) {
-            movie = "Mr. Nobody";
+    }
+};
+
+// Function for random command
+function doThis() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
         } else {
-            movie = search;
-            axios.get("http://www.omdbapi.com/?t=" + movie + "&apikey=trilogy")
-                .then(
-                    function (response) {
-                        var details = response.data;
-                        var movieResponse = "Title: " + details.Title + "\nYear: " + details.Year + "\nIMDB Rating: " + details.Ratings[0].Value + "\nRotten Tomatoes Rating: " + details.Ratings[1].Value + "\nCountry: " + details.Country + "\nLanguage: " + details.Language + "\nPlot: " + details.plot + "\nActors: " + details.Actors + "\n------------------------------\n";
-                        console.log(movieResponse);
-                        fs.appendFile("log.txt", movieResponse, function (err) {
-                            if (err) {
-                                return console.log(err);
-                            }
-                        })
-                    });
+            var randomArr = data.split(",");
+            search = randomArr[1];
+            spotifyThis();
         }
-        break;
-        
-    case "do-what-it-says":
-        console.log("whatever"); // function goes here
-        break;
-} 
+    });
+}
